8c919c5067c965844715d34c3e32b982b7a1a3d6
Bbaranoski <breno.b.o.baranoski@gmail.com>
2024-03-13 18:30:12 -0300

    fazendo a funcao fibonacci

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
+function verificaFibonacci(numero){
+    let fibonacci = [0, 1];
+    let i = 1;
 
-let indice = 13
-let soma = 0
-for(let k = 0; k < indice; k++){
-soma += k
+    while(fibonacci[i] <= numero){
+        fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+        i++;
+    }
+
+    if (fibonacci.includes(numero)) {
+        return console.log(`O número ${numero} pertence à sequência de Fibonacci.`);
+    } else {
+        return console.log(`O número ${numero} não pertence à sequência de Fibonacci.`);
+    }
 }
+
+verificaFibonacci(34)
